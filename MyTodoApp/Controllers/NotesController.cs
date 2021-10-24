a9d054abca1120ad9e75704dbc80ee0e59e76c00
JOHN ESPINAL <john_espinal@bhdleon.com.do>
2021-10-24 09:51:15 -0400

    Initial Commit

---
+++ MyTodoApp/Controllers/NotesController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyTodoApp;
+
+namespace MyTodoApp.Controllers
+{
+    public class NotesController : Controller
+    {
+        private TodoDBEntities db = new TodoDBEntities();
+
+        // GET: Notes
+        public ActionResult Index()
+        {
+            var note = db.Note.Include(n => n.Type).Include(n => n.User);
+            return View(note.ToList());
+        }
+
+        // GET: Notes/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Note note = db.Note.Find(id);
+            if (note == null)
+            {
+                return HttpNotFound();
+            }
+            return View(note);
+        }
+
+        // GET: Notes/Create
+        public ActionResult Create()
+        {
+            ViewBag.TypeId = new SelectList(db.Type, "Id", "TypeName");
+            ViewBag.UserId = new SelectList(db.User, "Id", "Name");
+            return View();
+        }
+
+        // POST: Notes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Seq,Title,Description,ReminderDate,TypeId,Status,UserId,CreationDate")] Note note)
+        {
+            if (ModelState.IsValid)
+            {
+                note.Id = Guid.NewGuid();
+                db.Note.Add(note);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.TypeId = new SelectList(db.Type, "Id", "TypeName", note.TypeId);
+            ViewBag.UserId = new SelectList(db.User, "Id", "Name", note.UserId);
+            return View(note);
+        }
+
+        // GET: Notes/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Note note = db.Note.Find(id);
+            if (note == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.TypeId = new SelectList(db.Type, "Id", "TypeName", note.TypeId);
+            ViewBag.UserId = new SelectList(db.User, "Id", "Name", note.UserId);
+            return View(note);
+        }
+
+        // POST: Notes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Seq,Title,Description,ReminderDate,TypeId,Status,UserId,CreationDate")] Note note)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(note).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.TypeId = new SelectList(db.Type, "Id", "TypeName", note.TypeId);
+            ViewBag.UserId = new SelectList(db.User, "Id", "Name", note.UserId);
+            return View(note);
+        }
+
+        // GET: Notes/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Note note = db.Note.Find(id);
+            if (note == null)
+            {
+                return HttpNotFound();
+            }
+            return View(note);
+        }
+
+        // POST: Notes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Note note = db.Note.Find(id);
+            db.Note.Remove(note);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MyTodoApp/Controllers/TypesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyTodoApp;
+
+namespace MyTodoApp.Controllers
+{
+    public class TypesController : Controller
+    {
+        private TodoDBEntities db = new TodoDBEntities();
+
+        // GET: Types
+        public ActionResult Index()
+        {
+            return View(db.Type.ToList());
+        }
+
+        // GET: Types/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Type type = db.Type.Find(id);
+            if (type == null)
+            {
+                return HttpNotFound();
+            }
+            return View(type);
+        }
+
+        // GET: Types/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Types/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Seq,TypeName,CreationDate")] Type type)
+        {
+            if (ModelState.IsValid)
+            {
+                type.Id = Guid.NewGuid();
+                db.Type.Add(type);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(type);
+        }
+
+        // GET: Types/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Type type = db.Type.Find(id);
+            if (type == null)
+            {
+                return HttpNotFound();
+            }
+            return View(type);
+        }
+
+        // POST: Types/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Seq,TypeName,CreationDate")] Type type)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(type).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(type);
+        }
+
+        // GET: Types/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Type type = db.Type.Find(id);
+            if (type == null)
+            {
+                return HttpNotFound();
+            }
+            return View(type);
+        }
+
+        // POST: Types/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Type type = db.Type.Find(id);
+            db.Type.Remove(type);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
